445dc7185360b008344d7b7947e0310ad7de478e
Sean Clark <sean@square-bracket.com>
2014-07-17 03:14:11 -0400

    Update ProgressHUD.m

--- src/ios/ProgressHUD.m
+++ src/ios/ProgressHUD.m
@@ -36,7 +36,7 @@
 		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		return;
 	}
-	self.progressHUD.progress = (float) progress;
+	self.progressHUD.progress = *progress;
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@""];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
