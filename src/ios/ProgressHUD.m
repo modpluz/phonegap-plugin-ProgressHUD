24d66ded794aaffe1ec782f1efaeced3c52c8007
Sean Clark <sean@square-bracket.com>
2014-07-17 03:05:11 -0400

    Update ProgressHUD.m

--- src/ios/ProgressHUD.m
+++ src/ios/ProgressHUD.m
@@ -30,7 +30,7 @@
 
 - (void)setValue:(CDVInvokedUrlCommand*)command
 {
-	float* progress = [command argumentAtIndex:0];
+	float progress = [command argumentAtIndex:0];
 
 	if (!self.progressHUD) {
 		CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
